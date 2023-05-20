27adfd4ad11341f6ae9f40b61f22332fedaf5246
35P10 <villanueva.guerrero@outlook.com>
2023-05-20 11:40:11 -0500

    add: new seed data

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -13,23 +13,36 @@ public static class SeedData {
                 DbContextOptions<AppDBContext>>()))
         {
             // Look for any negocios.
-            if (context.Negocio.Any())
-            {
-                return;   // DB has been seeded
+            if(!context.Negocio.Any()){
+                context.Negocio.AddRange(
+                    new Negocio{
+                        //Id = 1, database automatically assigns it.
+                        NombreEmpresa = "Negocio 01",
+                        Telefono = 123456789
+                    },
+                    new Negocio{
+                        NombreEmpresa = "Negocio 02",
+                        Telefono = 123456789
+                    }
+                );
             }
 
-            context.Negocio.AddRange(
-                new Negocio{
-                    //Id = 1, database automatically assigns it.
-                    NombreEmpresa = "Negocio 01",
-                    Telefono = 123456789
-                },
-                new Negocio{
-                    NombreEmpresa = "Negocio 02",
-                    Telefono = 123456789
-                }
-            );
-            
+            if(!context.Servicio.Any()){
+                context.Servicio.AddRange(
+                    new Servicio{
+                        //Id = 1, database automatically assigns it.
+                        Distrito = "Arequipa",
+                        Direccion = "Cabaña Maria",
+                        Nombre = "Servicio de limpieza del hogar profesional",
+                        Descripcion = "Descripcion!",
+                        HorarioInicio = new TimeSpan(2, 14, 18),
+                        HorarioFin = new TimeSpan(2, 14, 18),
+                        Categoria = "Limpieza del hogar",
+                        NegocioID = 1
+                    }
+                );
+            }
+
             context.SaveChanges();
         }
     }
