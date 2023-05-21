3cbeedec2ac4014e4771725fc90ba09071e17793
35P10 <villanueva.guerrero@outlook.com>
2023-05-20 19:22:04 -0500

    feat: update migrations

---
+++ Migrations/20230521001229_AddComentariosFavPedidos.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProHogarApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddComentariosFavPedidos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Estado",
+                table: "Pedido",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ServicioID",
+                table: "Pedido",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Pedido");
+
+            migrationBuilder.DropColumn(
+                name: "ServicioID",
+                table: "Pedido");
+        }
+    }
+}
