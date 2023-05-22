833967e2bc4d239085dcf75d3f931988dbcfd834
35P10 <villanueva.guerrero@outlook.com>
2023-05-21 23:59:16 -0500

    feat(api): add Servicio & search Servicio using filters

--- Controllers/ServiciosController.cs
+++ Controllers/ServiciosController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -94,5 +95,43 @@ namespace ProHogarApi.Controllers
                 }).ToListAsync();
         }
 
+        // POST: api/Negocio
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Servicio>> AddServicio(Servicio servicio) {
+            if (_context.Servicio == null){
+                return Problem("Entity set 'AppDBContext.Servicio'  is null.");
+            }
+            
+            _context.Servicio.Add(servicio);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetServicio", new { id = servicio.ServicioID }, servicio);
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<ServicioShortDescription>>> HandleSearch(string? keyword, string? distrito, int? calificacion) {
+            if (_context.Servicio == null){
+                return Problem("Entity set 'AppDBContext.Servicio'  is null.");
+            }
+
+            Expression<Func<Servicio,bool>> searchPred = s => 
+                ((keyword == null || s.Nombre == null) || s.Nombre.Contains(keyword)) &&
+                ((distrito == null || s.Distrito == null) || s.Distrito.Equals(distrito)) ;
+
+            return await _context.Servicio
+                .Where(searchPred)
+                .Join(_context.Negocio, 
+                    s => s.NegocioID,
+                    n => n.NegocioID, 
+                    (s, n) => new {Servicio = s, Negocio = n})
+                .Select(sn => new ServicioShortDescription{
+                    ServicioID = sn.Servicio.ServicioID,
+                    ServicioNombre = sn.Servicio.Nombre,
+                    ServicioDistrito = sn.Servicio.Distrito,
+                    ServicioCategoria = sn.Servicio.Categoria,
+                    NegocioNombre = sn.Negocio.NombreEmpresa
+                }).ToListAsync();
+        }
     }
 }
